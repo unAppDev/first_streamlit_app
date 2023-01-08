ddaeb735aa839b6bca84715324564c6f5ac5d7ba
unAppDev <122229235+unAppDev@users.noreply.github.com>
2023-01-09 06:35:08 +1100

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('- Omega 3 & Blueberry Oatmeal')
 streamlit.text('- Kale, Spinach & Rocket Smoothie')
 streamlit.text('- Hard-Boiled Free-Range Egg')
+streamlit.text('- Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
