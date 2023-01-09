9726638cdfb89b600b4f02e7c8458de1a369aaa6
unAppDev <122229235+unAppDev@users.noreply.github.com>
2023-01-10 09:16:29 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,16 @@ streamlit.dataframe(fruits_to_show) # Display the table on the page
 
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered: ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # JSON key value pairs to single values
-streamlit.dataframe(fruityvice_normalized) # Add values to table
+try:
+	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+		fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # JSON key value pairs to single values
+		streamlit.dataframe(fruityvice_normalized) # Add values to table
+except URLError as e:
+	streamlit.error()
 
 # don't run anything past here hwil we troubleshoot
 streamlit.stop()
